418d701a3b38a1a5a50a22dfaadb0b1488d6680d
decibe1magic <decibe1magic@gmail.com>
2021-06-28 11:19:03 +0900

    이용날짜, 주민번호 부분 수정

--- src/Everland.java
+++ src/Everland.java
@@ -14,13 +14,24 @@ public class Everland {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			System.out.println("이용날짜를 입력하세요.(숫자로만 8자리)");
 			String selectDate = sc.next();
+			if(selectDate.length()!=8) {
+				while(selectDate.length()!=8) {
+					System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+					selectDate=sc.next();
+				}
+			}
 			System.out.println("주민번호 앞자리를 입력하세요.(숫자로만 6자리)");
 			String regNum = sc.next();
+			if(regNum.length()!=6) {
+				while(regNum.length()!=6) {
+					System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+					regNum=sc.next();
+				}
+			}
 			System.out.println("몇 개를 주문하시겠습니까?");
 			int quantity = sc.nextInt();
 			System.out.println("우대사항을 선택하세요.\n1. 없음\n2. 장애인\n3. 국가유공자\n4. 다자녀\n5. 임산부");
