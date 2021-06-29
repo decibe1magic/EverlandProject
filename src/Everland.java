39389a213cb4f6da9bfcd9b4a11d7dec771472c1
decibe1magic <decibe1magic@gmail.com>
2021-06-29 11:31:20 +0900

    모듈화

--- src/Everland.java
+++ src/Everland.java
@@ -24,41 +24,68 @@ public class Everland {
 		ArrayList<Integer> preferentialNum = new ArrayList<Integer>();
 		while (true) {
 			count = 0;
-			selectDate.clear();
-			regNum.clear();
-			quantity.clear();
-			preferentialNum.clear();
+			clearAll(selectDate, regNum, quantity, preferentialNum);
 			do {
-				System.out.println("이용날짜를 입력하세요.(숫자로만 8자리)");
-				selectDate.add(sc.next());
-				if (selectDate.get(count).length() != 8) {
-					while (selectDate.get(count).length() != 8) {
-						selectDate.remove(count);
-						System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-						selectDate.add(sc.next());
-					}
-				}
-				System.out.println("주민번호 앞자리를 입력하세요.(숫자로만 6자리)");
-				regNum.add(sc.next());
-				if (regNum.get(count).length() != 6) {
-					while (regNum.get(count).length() != 6) {
-						regNum.remove(count);
-						System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-						regNum.add(sc.next());
-					}
-				}
-				System.out.println("몇 개를 주문하시겠습니까?");
-				quantity.add(sc.nextInt());
-				System.out.println("우대사항을 선택하세요.\n1. 없음\n2. 장애인\n3. 국가유공자\n4. 다자녀\n5. 임산부");
-				preferentialNum.add(sc.nextInt());
-
+				inputDate(selectDate,count);
+				inputRegNum(regNum,count);
+				inputQuantity(quantity,count);
+				inputPreferentialNum(preferentialNum,count);
 				count++;
-
 				System.out.println("추가 : 0, 종료 : 1");
 				endSequence = sc.nextInt();
 			} while (endSequence != 1);
 			PrintPrice(selectDate, regNum, quantity, preferentialNum, count);
 		}
+	}
+	
+	public static ArrayList<String> inputDate(ArrayList<String> selectDate, int count) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("이용날짜를 입력하세요.(숫자로만 8자리)");
+		selectDate.add(sc.next());
+		if (selectDate.get(count).length() != 8) {
+			while (selectDate.get(count).length() != 8) {
+				selectDate.remove(count);
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+				selectDate.add(sc.next());
+			}
+		}
+		return selectDate;
+	}
+	
+	public static ArrayList<String> inputRegNum(ArrayList<String> regNum, int count) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("주민번호 앞자리를 입력하세요.(숫자로만 6자리)");
+		regNum.add(sc.next());
+		if (regNum.get(count).length() != 6) {
+			while (regNum.get(count).length() != 6) {
+				regNum.remove(count);
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+				regNum.add(sc.next());
+			}
+		}
+		return regNum;
+	}
+	
+	public static ArrayList<Integer> inputQuantity(ArrayList<Integer> quantity, int count) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("몇 개를 주문하시겠습니까?");
+		quantity.add(sc.nextInt());
+		return quantity;
+	}
+	
+	public static ArrayList<Integer> inputPreferentialNum(ArrayList<Integer> preferentialNum, int count) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("우대사항을 선택하세요.\n1. 없음\n2. 장애인\n3. 국가유공자\n4. 다자녀\n5. 임산부");
+		preferentialNum.add(sc.nextInt());
+		return preferentialNum;
+	}
+
+	public static void clearAll(ArrayList<String> selectDate, ArrayList<String> regNum, ArrayList<Integer> quantity,
+			ArrayList<Integer> preferentialNum) {
+		selectDate.clear();
+		regNum.clear();
+		quantity.clear();
+		preferentialNum.clear();
 	}
 
 	public static void PrintPrice(ArrayList<String> selectDate, ArrayList<String> regNum, ArrayList<Integer> quantity,
